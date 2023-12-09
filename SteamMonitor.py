5cf838d70c63f60f5fd88a842eb517d9b1f16cb4
QwertyZaq <53946720+Cryptinum@users.noreply.github.com>
2023-12-09 23:51:09 +0800

    Update SteamMonitor.py
    ver 0.1

--- SteamMonitor.py
+++ SteamMonitor.py
@@ -1,46 +1,56 @@
-import warnings, requests
+import warnings, requests, time
 from bs4 import BeautifulSoup
 
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 from datetime import datetime
-import ciso8601, time
 
-# Steam ID for monitoring
-steamID = "shiranaiwa"
+from getstatus import *
 
 # Global Settings
-np.set_printoptions(suppress=True)
+# np.set_printoptions(suppress=True)
 warnings.filterwarnings("ignore")  # Suppress warnings.
-pd.options.display.max_rows = 10  # Display no more than 10 rows.
+# pd.options.display.max_rows = 10  # Display no more than 10 rows.
+
+# User for monitoring
+steamID = "shiranaiwa"
+period = 1
+
 
 urlTemplate = "https://steamcommunity.com/id/{}"
 url = urlTemplate.format(steamID)
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 }
-page = requests.get(url, headers=headers, verify=False)  # Do not verify
-page.encoding = "utf-8"
-soup = BeautifulSoup(page.text, "html.parser")
 
-# Current time
-now = datetime.now()
+previousStatus = [None for _ in range(3)]
 
-print(now)
-print(forteenDaysTotal(soup))
-print(statusNow(soup))
-print(gameNow(soup))
+while True:
+    try:
+        # Request user page
+        page = requests.get(url, headers=headers, verify=False)  # Do not verify
+        page.encoding = "utf-8"
+        soup = BeautifulSoup(page.text, "html.parser")
 
-def forteenDaysTotal(soup):
-    '''Total game time in last 14 days, 0 if fail to get element.'''
-    source = soup.find_all("div", class_="recentgame_quicklinks recentgame_recentplaytime"    )
-    return float(source[0].text.strip()[:-19]) if source else 0.0
+        if page.status_code is 200:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M")
+            total = forteenDaysTotal(soup)
+            status = statusNow(soup)
+            game = gameNow(soup)
+            nowStatus = [total, status, game]
 
-def statusNow(soup):
-    '''Current status.'''
-    return soup.find_all("div", class_="profile_in_game_header")[0].text.strip()
+            with open(steamID+'_StatusLog.csv','a') as file:
+                file.write(f'{now},{total},{status},{game}\n')
+            if nowStatus != previousStatus:
+                with open(steamID+'_ChangeLog.csv', 'a') as changeFile:
+                    changeFile.write(f'{now},{total},{status},{game}\n')
+                print(f'[{now}],{total},{status} - {game}')
+                previousStatus = nowStatus
+            else:
+                print(f'[{now}],{total},{status} - {game}')
+        else:
+            print(f'Failed to retrieve data for {steamID}. Status Code: {page.status_code}')
 
-def gameNow(soup):
-    ''' Current game, empty string if fail to get element (no game playing).'''
-    source = soup.find_all("div", class_="profile_in_game_name")
-    return source[0].text.strip() if source else ""
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred for {steamID}: {e}")
+    time.sleep(period * 60)
