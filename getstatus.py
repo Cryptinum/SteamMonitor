08912314aa84df3ff2be8204443d51a9f9520bf1
QwertyZaq <53946720+Cryptinum@users.noreply.github.com>
2023-12-09 23:51:55 +0800

    Add files via upload
    ver 0.1

---
+++ getstatus.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+
+def forteenDaysTotal(soup):
+    """Total game time in last 14 days, 0 if fail to get element."""
+    source = soup.find_all("div", class_="recentgame_quicklinks recentgame_recentplaytime")
+    return float(source[0].text.strip()[:-19]) if source else 0.0
+
+
+def statusNow(soup):
+    """Current status."""
+    return soup.find_all("div", class_="profile_in_game_header")[0].text.strip()
+
+
+def gameNow(soup):
+    """Current game, empty string if fail to get element (no game playing)."""
+    source = soup.find_all("div", class_="profile_in_game_name")
+    return source[0].text.strip() if source else ""
